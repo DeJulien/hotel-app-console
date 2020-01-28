1e1b267b573e1ee511b8d374b17eda7451733eac
Julien Dewost <dewostjulien@gmail.com>
2020-01-28 17:02:58 +0100

    tp1 maj4

--- presentation.js
+++ presentation.js
@@ -3,7 +3,8 @@ var service = require('./service');
 function start()
 {
     console.log('1. Lister les clients');
-    console.log('2. Ajouter client')
+    console.log('2. Ajouter client');
+    console.log('3. Rechercher un client par nom');
     console.log('99. Sortir');
 
     var readline = require('readline');
@@ -12,6 +13,7 @@ function start()
         input: process.stdin,
         outpt: process.stdout
     })
+    
 
     choix.question('saisissez un numéro : ', function (saisie) {
         if (saisie === '99') {
@@ -33,10 +35,34 @@ function start()
         } 
         else if(saisie === '2'){
             console.log('***Ajouter client***');
-            service.AjouterClient(function(data){
-                start();
+            choix.question('saisissez un nom : ', function (saisieN) {
+                var nom=saisieN;
+                choix.question('saisissez un prenom : ', function (saisieP) {
+                    var prenom=saisieP;
+                    console.log('nom : '+ nom);
+                    console.log('prenom : '+ prenom);
+                    service.AjouterClient(nom,prenom)
+                    start();
+                });
             });
-            
+        }
+        else if(saisie === '3'){
+            console.log('***Rechercher un client***');
+                service.trouverClient(function(data){
+                    choix.question('saisissez un nom : ', function (saisieC) {
+                        var nom=saisieC;
+                    data.forEach(function(client) {
+                        if((client.nom).toLowerCase()===nom)
+                        {
+                        console.log(client.uuid);
+                        console.log(client.nom);
+                        console.log(client.prenoms);
+                        console.log('**********');
+                        }
+                    });
+                    start();
+                });
+            });
         }
     })
 }
